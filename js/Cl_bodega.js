0713a96448d2f86429f0b85dadc763d49030b197
MVCamachoSanchez <mariavalecamacho1105@gmail.com>
2025-02-04 18:50:44 -0400

    cambio1

--- js/Cl_bodega.js
+++ js/Cl_bodega.js
@@ -2,4 +2,12 @@ export default class Cl_bodega{
     constructor(){
         this.acmGanancia = 0;
     }
+
+    procesarArticulo(a){
+        this.acmGanancia += a.ganancia();
+    }
+
+    totalGanancia(){
+        return this.acmGanancia;
+    }
 }
